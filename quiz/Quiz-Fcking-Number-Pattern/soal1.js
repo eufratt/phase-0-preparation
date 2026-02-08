67d90db1be53ab4557b5b80a6f6b1b281346d559
eufratt <ayyasheufrat2@gmail.com>
2026-02-08 19:57:35 +0700

    quiz number done

---
+++ quiz/Quiz-Fcking-Number-Pattern/soal1.js
@@ -0,0 +1,67 @@
+// Soal 1
+// ```js
+/*
+================
+MISSING NUMBER
+================
+description: Sebuah fungsi untuk mencari angka yang hilang berdasarkan pola dari board atau
+papan yang tersedia. Fungsi akan mengembalikan nilai sebuah array yang berisi
+angka-angka yang hilang
+
+examples:
+INPUT = 
+[
+  [ 7 ,' ', 5 ],
+  [' ', 8 , 9 ]
+  [ 1 ,' ',' '] 
+]
+
+ASUMSI PADA PAPAN SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL YAITU 1 DAN 9 SEHINGGA
+OUTPUT:
+[ 2, 3, 4, 6]
+
+PADA MASING-MASING TEST CASE SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL
+*/
+
+function missingNum(arr) {
+  //code here
+  let nums = []
+  for (const x of arr) {
+    for (const y of x) {
+        if(typeof y === "number") nums.push(y);
+    }
+  }
+  if(nums.length === 0)return [];
+
+  const min = Math.min(...nums);
+  const max = Math.max(...nums);
+
+  let missing = [];
+  for(let x = min; x <= max; x++){
+    if(!nums.includes(x)) missing.push(x);
+  }
+  return missing;
+}
+
+
+console.log(missingNum([
+  [3, ' ', 5],
+  [1, ' ', 7],
+  [9, ' ', ' ']
+])) // [ 2, 4, 6, 8 ]
+console.log(missingNum([
+  [2, ' '],
+  [' ', 5]
+])) // [ 3, 4 ]
+console.log(missingNum([
+  [11, ' ', 13],
+  [17, ' ', 19],
+  [' ', 16, ' ']
+])) // [ 12, 14, 15, 18 ]
+console.log(missingNum([
+  [3, ' ', 5, 15],
+  [1, ' ', 7, 13],
+  [9, ' ', ' ', 12],
+  [' ', 16, ' ', ' ']
+])) // [ 2, 4, 6, 8, 10, 11, 14 ]
+console.log(missingNum([])) // []
