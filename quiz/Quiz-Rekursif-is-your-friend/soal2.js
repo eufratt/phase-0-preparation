c9a655ccb56e64badba49e80d8b881248c9759c0
eufratt <ayyasheufrat2@gmail.com>
2026-02-28 22:06:08 +0700

    rekursif done

---
+++ quiz/Quiz-Rekursif-is-your-friend/soal2.js
@@ -0,0 +1,35 @@
+// Soal 2
+// ```js
+/**
+ * ////////////////
+ * changeXRecursive
+ * ////////////////
+ * Function ini harus dikerjakan dengan menggunakan rekursif.
+ * Function ini menerima dua parameter, yaitu:
+ *  1. data yang merupakan string dari number
+ *  2. jenis yang merupakan string berisi antara ganjil dan genap
+ *
+ * [EXAMPLE]
+ * Input: data = 012345678922468 dan jenis = ganjil
+ * Process: Karena jenisnya ganjil maka setiap bilangan genap diubah menjadi x
+ * Output: x1x3x5x7x9xxxxx
+ *
+ * [RULES]
+ * 1. Dilarang mengubah tipe parameter function
+ * 2. Dilarang membuat function diluar function yang disediakan
+ * 5. Wajib menggunakan rekursif
+ */
+
+function changeXRecursive(data, jenis) {
+  //code here
+  let n = Number(data[0]);
+  let result =
+    (jenis === "ganjil" && n % 2 === 0) || (jenis === "genap" && n % 2 !== 0)
+      ? (n = "x")
+      : data[0];
+  if (data.length === 1) return result;
+  return result + changeXRecursive(data.slice(1), jenis);
+}
+
+console.log(changeXRecursive("012345678922468", "ganjil")); //x1x3x5x7x9xxxxx
+console.log(changeXRecursive("0123456789", "genap")); //0x2x4x6x8x
